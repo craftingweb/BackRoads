02339e6487986952400d62a0f44486923c17196b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-01-24 19:19:58 -0500

    mobile friendly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,19 @@
-import { socialLinks } from "../data";
+import { pageLinks, socialLinks } from "../data";
 const Footer = () => {
   return (
     <footer className="section footer">
       <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className="footer-link">
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
+
       <ul className="footer-icons">
         {socialLinks.map((social) => {
           const { id, href, icon } = social;
@@ -38,7 +28,7 @@ const Footer = () => {
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   );
